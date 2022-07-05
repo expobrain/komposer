95773da969267a0575ddda5b9ce51fccdd5d96bf
Daniele Esposti <daniele@withplum.com>
2022-07-05 18:52:24 +0100

    Added support for restart policy

--- komposer/types/docker_compose.py
+++ komposer/types/docker_compose.py
@@ -1,3 +1,4 @@
+from enum import Enum, unique
 from pathlib import Path
 from typing import Optional, Union
 
@@ -10,8 +11,17 @@ EnvironmentArray = list[str]
 Environment = Union[EnvironmentMap, EnvironmentArray]
 
 
+@unique
+class RestartPolicy(Enum):
+    NO = "no"
+    ALWAYS = "always"
+    ON_FAILURE = "on-failure"
+    UNLESS_STOPPED = "unless-stopped"
+
+
 class Service(ImmutableBaseModel):
     image: Optional[str] = None
+    restart: Optional[RestartPolicy] = None
     ports: list[str] = []
     command: Optional[Union[str, list[str]]] = None
     env_file: Optional[Path] = None
